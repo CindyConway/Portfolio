6859754b8d436b006a66d04207f04736a7651b69
Cindy Conway <cindyannconway@gmail.com>
2016-10-23 21:35:05 -0700

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+import { CandidateComponent } from './candidate/candidate.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { VideosComponent } from './videos/videos.component';
+import { FooterComponent } from './footer/footer.component';
+import { SamplesComponent } from './samples/samples.component';
+
+import { AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CandidateComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    VideosComponent,
+    FooterComponent,
+    SamplesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AlertModule
   ],
   providers: [],
   bootstrap: [AppComponent]
